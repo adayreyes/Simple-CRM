b8cfe0f75aba7b3358420fc1032b88cd47e14e02
Aday Reyes <adayreyes4a@gmail.com>
2022-06-01 18:12:16 +0200

    rendering data from firebase

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import {MatDialog} from '@angular/material/dialog';
 import { AddUserDialogComponent } from '../add-user-dialog/add-user-dialog.component';
+import { initializeApp } from "firebase/app";
+import { Firestore, getFirestore, collection, getDocs } from "firebase/firestore";
+
 
 @Component({
   selector: 'app-user',
@@ -9,13 +12,37 @@ import { AddUserDialogComponent } from '../add-user-dialog/add-user-dialog.compo
 })
 export class UserComponent implements OnInit {
 
- 
+  firebaseConfig: any = {
+    apiKey: "AIzaSyBUoLE1rdno3i4ohOH7gkCe4ESudwRYJ3M",
+    authDomain: "simple-crm-7ccda.firebaseapp.com",
+    projectId: "simple-crm-7ccda",
+    storageBucket: "simple-crm-7ccda.appspot.com",
+    messagingSenderId: "909531329180",
+    appId: "1:909531329180:web:db649327e6bc562017f359"
+  };
+  app = initializeApp(this.firebaseConfig);
+  db = getFirestore(this.app);
+  allUsers: any = [];
+  
   constructor(public dialog: MatDialog) { }
-
+  
   ngOnInit(): void {
+    this.showChanges()
   }
+
+  async showChanges() {
+  let docs = await getDocs(collection(this.db, "users"));
+  docs.forEach(doc => {
+    this.allUsers.push(doc.data());
+    console.log(this.allUsers);
+    
+  });
+  
+  
+}
 
   openDialog(){
     this.dialog.open(AddUserDialogComponent)
   }
+
 }
